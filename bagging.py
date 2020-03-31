6207b2c50b70101bf98d6d0719cb705cbe3c1160
Ryan Jones <ryan.wesley.jones@gmail.com>
2020-03-30 20:47:34 -0400

    Added regressor function to bagging.py

--- bagging.py
+++ bagging.py
@@ -14,7 +14,6 @@ __copyright__ = "Copyright March 2020, UCF MSDA Program"
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble import BaggingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import cross_val_score
@@ -25,6 +24,14 @@ def load_file(filepath):
     Load the csv file into a dataframe
     '''
     return pd.read_csv(filepath)
+
+
+def split_X_and_y(dataframe):
+    '''
+    Returns X dataframe and y series representing predictors and response sets, respectively.
+    Note: Assumes the response feature is in the last column.
+    '''
+    return dataframe.iloc[:,:-1], dataframe.iloc[:,-1]
 
 
 def bootstrap_sample(dataframe, sample_size=1460):
@@ -38,3 +45,21 @@ def bootstrap_sample(dataframe, sample_size=1460):
 
     # randomly sample "sample_size" values with replacement from "dataframe"
     return dataframe.sample(n=sample_size, replace=True, axis=0)
+
+
+def regressor(dataframe, estimator=LinearRegression(), n_estimators=20):
+    '''
+    Perform regression on the dataframe (as defined by "estimator") n_estimator times. Return dataframe of predictions for each iteration.
+    '''
+    # initialize an empty dataframe to store predicted values
+    predictions = pd.DataFrame()
+
+    # perform regression with bagging and store the predictions
+    for i in range(n_estimators):
+        df_temp = bootstrap_sample(dataframe)
+        X, y = split_X_and_y(df_temp)
+        reg = estimator.fit(X,y)
+        predict = reg.predict(X)
+        predictions['Estimator_'+str(i+1)] = predict
+
+    return predictions
